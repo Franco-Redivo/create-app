bfccd8be3d06466bb7119f6a6c756f5a4cc97ae3
Franco-Redivo <franco161101@gmail.com>
2026-04-21 14:44:27 -0300

    feat: add GitHub workflows for periodic health checks, pipeline, and Playwright tests

--- server/util/routes.js
+++ server/util/routes.js
@@ -3,6 +3,10 @@ const messages = require('@controllers/messagesController')
 
 const router = Router()
 
+router.get('/health', (req, res) => {
+	res.send('ok')
+})
+
 router.get('/messages', messages.getAll)
 router.post('/messages', messages.create)
 router.delete('/messages/:id', messages.destroy)
